120c5d2566f522a0c74a07340dc3f4056db4c7eb
Georgiy Kuklin <g.kuklin@g.nsu.ru>
2023-11-24 11:21:17 +0700

    Every process in it's own process group. Foreground not foreground all that stuff

--- 22213/g.kuklin/shell/shell.c
+++ 22213/g.kuklin/shell/shell.c
@@ -17,16 +17,24 @@
 struct command cmds[MAXCMDS];
 char bkgrnd;
 
-int active_job = -1;
-
 void send_to_active(int); 
 
-void execute_command(int id) {
+void execute_command(int id, int shell_terminal) {
+    signal(SIGINT, SIG_DFL);
+    signal(SIGQUIT, SIG_DFL);
+    signal(SIGTTOU, SIG_DFL);
+
+    pid_t pid = getpid();
+    if (setpgid(pid, pid) != 0) {
+        perror("Failed to set child process group. from child");    
+    }
+    if (!bkgrnd && tcsetpgrp(shell_terminal, pid) != 0) {
+        perror("Failed to set new pg a foreground process group. from child");
+    }
     if (cmds[id].cmdflag & (OUTFILE | OUTFILEAP)) {
         int out;
         if (cmds[id].cmdflag & OUTFILE)
-            out = open(cmds[id].outfile, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
-        else
+            out = open(cmds[id].outfile, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH); else
             out = open(cmds[id].outfile, O_WRONLY | O_APPEND);
 
         if (out == -1) {
@@ -54,6 +62,28 @@ void execute_command(int id) {
     exit(1);
 }
 
+int add_job(pid_t job, pid_t *jobs, int *next_job) {
+    if (*next_job == -1) {
+        printf("Out of job slots");
+        return -1;
+    }
+    printf("[%d] %d\n", *next_job, job);
+    jobs[*next_job] = job;
+    // Find next free job handle
+    int initial = *next_job;
+    (*next_job)++;
+    while (jobs[*next_job] != 0 && *next_job != initial) {
+        (*next_job)++;
+        if (*next_job == JOBS_BUFFER_SIZE) {
+            *next_job = 0;
+        }
+    }
+    if (*next_job == initial) {
+        *next_job = -1; // There is no empty slot
+    }
+    return 0;
+}
+
 int main() {
     register int i;
     char line[1024];      /*  allow large command lines  */
@@ -63,21 +93,22 @@ int main() {
     // TODO(theblek): make this a linked list of jobs by encoding next free as a negative number
     pid_t jobs[JOBS_BUFFER_SIZE] = {0};
     int next_job = 0;
-    int active_job = -1;
 
-    // Register signal handlers
-    struct sigaction sigint = {
-        .sa_handler = send_to_active,
-        .sa_flags = 0,
-    };
-    if (sigaction(SIGINT, &sigint, NULL) == -1) {
-        perror("Failed to install new signal handlers");
-        return -1;
+    pid_t shell_pgid = getpid();
+    int shell_terminal = STDIN_FILENO;
+    assert(isatty(shell_terminal));
+
+    if (setpgid(shell_pgid, shell_pgid) != 0) {
+        perror("Couldn't put shell into it's own process group");
     }
-    if (sigaction(SIGQUIT, &sigint, NULL) == -1) {
-        perror("Failed to install new signal handlers");
-        return -1;
+    if (tcsetpgrp(shell_terminal, shell_pgid) != 0) {
+        perror("Failed to take control over terminal");
     }
+
+    // TODO(theblek): Rewrite for empty signal handler that repeats itself
+    signal(SIGINT, SIG_IGN);
+    signal(SIGQUIT, SIG_IGN);
+    signal(SIGTTOU, SIG_IGN);
 
     sprintf(prompt,"[shell] ");
 
@@ -128,43 +159,28 @@ int main() {
                     perror("Failed to fork");
                     return 1; 
                 case 0:
-                    execute_command(i);
+                    /* This is a child process */
+                    execute_command(i, shell_terminal);
                     break;
                 default:
+                    /* This is a shell process */
+                    if (setpgid(child, child) != 0) {
+                        perror("Failed to set child process group");    
+                    }
                     if (bkgrnd) {
-                        if (next_job == -1) {
-                            printf("Out of job slots");
-                        }
-                        printf("[%d] %d\n", next_job, child);
-                        jobs[next_job] = child;
-                        // Find next free job handle
-                        int initial = next_job;
-                        next_job++;
-                        while (jobs[next_job] != 0 && next_job != initial) {
-                            next_job++;
-                            if (next_job == JOBS_BUFFER_SIZE) {
-                                next_job = 0;
-                            }
-                        }
-                        if (next_job == initial) {
-                            next_job = -1; // There is no empty slot
-                        }
+                        add_job(child, jobs, &next_job);
                     } else {
-                        active_job = child;
-                        tc
+                        if (tcsetpgrp(shell_terminal, child) != 0) {
+                            perror("Failed to set new pg a foreground process group");
+                        }
                         if (waitid(P_PID, child, &child_info, WEXITED) == -1 && errno != EINTR) {
                             perror("Failed to wait for child");
                         }
-                        active_job = -1;
+                        if (tcsetpgrp(shell_terminal, shell_pgid) != 0) {
+                            perror("Failed to set shell to foreground");
+                        }
                     }
             }
         }
     }/* close while */
-}
-
-void send_to_active(int signal) {
-    if (active_job < 0) {
-        return;
-    }
-    sigsend(P_PID, active_job, signal);
 }
